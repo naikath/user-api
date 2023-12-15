82a18e5df3ecb95a66304a395dcab3027b92c8fb
Nicolas <138610419+naikath@users.noreply.github.com>
2023-12-15 01:17:55 -0300

    refactor(model): Improve types and rename them
    Splitted up the existing type for individual use

--- src/models/user.model.types.ts
+++ src/models/user.model.types.ts
@@ -1,10 +1,26 @@
-export type ResultModelObject =
-	| {
-			success: true
-	  }
-	| {
-			success: false
-			error: string
-	  }
+// Success Result
 
-export type ResultModel = Promise<ResultModelObject>
+export type ResultObjectSuccess = {
+	success: true
+}
+
+export type ResultPromiseSuccess = Promise<ResultObjectSuccess>
+
+// Error Result
+
+export type ResultObjectError = {
+	success: false
+	error: string
+}
+
+export type ResultPromiseError = Promise<ResultObjectError>
+
+// Common Result
+
+export type ResultObjectCommon = ResultObjectSuccess | ResultObjectError
+
+export type ResultPromiseCommon = Promise<ResultObjectCommon>
+
+// Model Result
+
+export type ResultModel = ResultPromiseCommon
